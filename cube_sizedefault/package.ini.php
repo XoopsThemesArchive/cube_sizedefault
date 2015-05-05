40f557e3a89430fe03afeb259bfd03917b8f5cbf
mambax7 <mambax7@gmail.com>
2015-05-05 08:56:31 -0400

    Original files

---
+++ cube_sizedefault/package.ini.php
@@ -0,0 +1,17 @@
+<?php
+/**
+
+[Manifesto]
+Name="Default Theme-SIZE (Resizing) Version"
+Depends=Legacy_RenderSystem
+Url="http://xoopscube.org and http://www.wanisys.net/"
+Version="2.00"
+
+[Theme]
+Author=XOOPS Cube Project Team and Wanikoo  and Authors of rico and prototype
+ScreenShot="screenshot.png"
+Description="Default theme-SIZE (Resizing) Version of XOOPS Cube 2.1"
+W3C=NG
+
+*/
+?>
